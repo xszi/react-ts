2720ea94bb0cfc4f120fc4afdf3364e8cab6f57e
DESKTOP-JIUHUA\jiuhua-pc <jiuhuaw@126.com>
2020-06-30 14:50:29 +0800

    add tabbar

---
+++ cnodejs/src/components/tabbar/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import TabbarWrapper, { TabbarItem } from './style'
+
+export interface Tabber {
+    name: string;
+    route: string;
+}
+
+interface IProps {
+    value: Array<Tabber>
+}
+
+const Tabber: React.FC<IProps> = (props: IProps) => {
+    return (
+        <TabbarWrapper>
+            {props.value.map((item) => {
+                return (
+                    <NavLink
+                        key={item.name}
+                        to={item.route}
+                        activeClassName={"active"}
+                    >
+                        <TabbarItem>{item.name || ""}</TabbarItem>
+                    </NavLink>
+                )
+            })}
+        </TabbarWrapper>
+    )
+}
+
+export default React.memo(Tabber)
--- cnodejs/src/layouts/base-layout/index.tsx
+++ cnodejs/src/layouts/base-layout/index.tsx
@@ -2,14 +2,25 @@ import React from 'react'
 // import { Switch, Route, Redirect } from 'react-router-dom'
 
 import Header from '../../components/header'
+import Tabbar, { Tabber } from '../../components/tabbar'
 // import Layout, { Fixed, Main } from './style'
 import Layout, { Fixed } from './style'
+
+const navList: Tabber[] = [
+    { name: '全部', route: '/topic/all'},
+    { name: '精华', route: '/topic/good'},
+    { name: '分享', route: '/topic/share'},
+    { name: '问答', route: '/topic/ask'},
+    { name: '招聘', route: '/topic/job'},
+    { name: '关于', route: '/about'},
+]
 
 const BaseLayout = () => {
     return (
         <Layout>
             <Fixed>
                 <Header logo={require('../../assets/logo.svg')}></Header>
+                <Tabbar value={navList}></Tabbar>
             </Fixed>
         </Layout>
     )
